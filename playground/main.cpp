7d432e8b702f64ada979dff810e46e964f1a0c0e
shohan11d <shohan11d@gmail.com>
2025-08-22 20:19:07 +0600

    module 3 added

--- playground/main.cpp
+++ playground/main.cpp
@@ -1,65 +1,39 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-vector<int> adj_list[1005];
-bool vis[1005];
-int level[1005];
-int parent[1005];
+char grid[100][105];
+bool vis[105][105];
 
-void bfs(int src)
-{
-    queue<int> q;
-    q.push(src);
-    vis[src] = true;
-    level[src] = 0;
-
-    while(!q.empty())
-    {
-        int par = q.front();
-        q.pop();
-
-        for(int child : adj_list[par])
-        {
-            if(!vis[child])
-            {
-                q.push(child);
-                vis[child] = true;
-                level[child] = level[par] + 1;
-                parent[child] = par;
-            }
-        }
-    }
+vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, m;
+bool valid(int i, int j) {
+  if (i < 0 || i >= n || j < 0 || j >= m) return false;
+  return true;
 }
+void dfs(int si, int sj) {
+  // base case
+  cout << si << " " << " " << endl;
+  vis[si][sj] = true;
 
-int main()
-{
-    int n,e;
-    cin >> n >> e;
-    while(e--)
-    {
-        int a,b;
-        cin >> a >> b;
-        adj_list[a].push_back(b);
-        adj_list[b].push_back(a);
-    }
-    memset(vis,false,sizeof(vis));
-    memset(level,-1,sizeof(level));
-    memset(parent,-1,sizeof(parent));
-    int src,dst;
-    cin >> src >> dst;
-    bfs(src);
+  for (int i = 0; i < 4; i++) {
+    // si sj
 
-    vector<int> path;
-    int node = dst;
-    while(node != -1)
-    {
-        path.push_back(node);
-        node = parent[node];
+    int ci = si + d[i].first;
+    int cj = sj + d[i].second;
+    if (valid(ci, cj) == true && !vis[ci][cj]) {
+      dfs(ci, cj);
+    };
+  }
+}
+int main() {
+  cin >> n >> m;
+  memset(vis, false, sizeof(vis));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < m; j++) {
+      cin >> grid[i][j];
     }
-    reverse(path.begin(),path.end());
-    for(int x : path)
-    {
-        cout << x << " ";
-    }
-    
-    return 0;
+  }
+
+  int si, sj;
+  cin >> si >> sj;
+  dfs(si, sj);
 }
